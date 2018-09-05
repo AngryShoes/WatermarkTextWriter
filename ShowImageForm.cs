ed9c5808920e6294647c61c01a04ddf12154654c
AngryShoes <284310681@qq.com>
2018-09-05 22:59:45 +0800

    add first commit

---
+++ ShowImageForm.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+
+namespace WaterMarkDemo
+{
+    public partial class ShowImageForm : Form
+    {
+        public ShowImageForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sourceImagePath = "https://i.loli.net/2018/09/05/5b8fc930eb809.bmp";
+            WebRequest webRequest = WebRequest.Create(sourceImagePath);
+            WebResponse webResponse = webRequest.GetResponse();
+            Stream imageStream = webResponse.GetResponseStream();
+            byte[] imageBytes = new byte[1024];
+            int length = 0;
+            Image sourceImage = null;
+            using (MemoryStream ms = new MemoryStream())
+            {
+                while ((length = imageStream.Read(imageBytes, 0, imageBytes.Length)) > 0)
+                {
+                    ms.Write(imageBytes, 0, length);
+                }
+                sourceImage = Image.FromStream(ms);
+            }
+            if (sourceImage != null)
+            {
+                string newPath = Application.StartupPath + "\\WaterMarkImage.png";
+                bool isGenerateSuccess = WatermarkTextWriter.WriteWaterMarkText(newPath, "HelloWold", "LightGray", 200, sourceImage);
+                if (isGenerateSuccess && File.Exists(newPath))
+                {
+                    pictureBox2.Image = Image.FromFile(newPath);
+                    return;
+                }
+                else
+                {
+                    MessageBox.Show("The generated image path is not exist!");
+                    return;
+                }
+            }
+        }
+    }
+}
---
+++ WatermarkTextWriter.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WaterMarkDemo
+{
+    internal class WatermarkTextWriter
+    {
+        /// <summary>
+        /// Used for write text as watermark in source image
+        /// </summary>
+        /// <param name="savePath"> Generated iamge file path</param>
+        /// <param name="waterText">Text display in image</param>
+        /// <param name="color">The color of text</param>
+        /// <param name="alpha">alpha is less than 0 or greater than 255</param>
+        /// <param name="sourceImage">source image used for generate new iamge with watermark, [Optional]</param>
+        /// <param name="oldPath">source image used for generate new image with watermark,[Optiona]</param>
+        /// <returns></returns>
+        public static bool WriteWaterMarkText(string savePath, string waterText, string color, int alpha, Image sourceImage = null, string oldPath = null)
+        {
+            Image image = string.IsNullOrEmpty(oldPath) ? sourceImage : Image.FromFile(oldPath);
+            Bitmap bitmap = new Bitmap(image.Width, image.Height);
+            Graphics graphics = Graphics.FromImage(bitmap);
+            graphics.Clear(Color.Transparent);
+            graphics.DrawImage(image, new Rectangle(0, 0, image.Width, image.Height), 0, 0, image.Width, image.Height,
+                GraphicsUnit.Pixel);
+            Font font = new Font("arial", 14);
+            Tuple<bool> tuple = Tuple.Create(false);
+
+            try
+            {
+                StringFormat stringFormat = new StringFormat
+                {
+                    Alignment = StringAlignment.Center,
+                    LineAlignment = StringAlignment.Center
+                };
+                SolidBrush solidBrush = new SolidBrush(Color.FromArgb(alpha, ColorTranslator.FromHtml(color)));
+                float x = 10F, y = 20F;
+                float width = 120.0F;
+                float height = 40.0F;
+
+                for (int i = 0; i < 5; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        for (int k = 0; k < 2; k++)
+                        {
+                            RectangleF drawRect = new RectangleF(x, y, width, height);
+                            graphics.RotateTransform(-8.0f);
+                            graphics.DrawString(waterText, font, solidBrush, drawRect, stringFormat);
+                            graphics.ResetTransform();
+                            x = x + 120.0F;
+                        }
+                        y += 35f;
+                    }
+                    x = 0f;
+                }
+
+                solidBrush.Dispose();
+                bitmap.Save(savePath, System.Drawing.Imaging.ImageFormat.Png);
+                tuple = Tuple.Create(true);
+            }
+            catch (Exception e)
+            {
+                tuple = Tuple.Create(false);
+            }
+            finally
+            {
+                bitmap.Dispose();
+                image.Dispose();
+            }
+            return tuple.Item1;
+        }
+    }
+}
